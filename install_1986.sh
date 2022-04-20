2e7b5b3dbc6626e0e096c3440e60592181aeed18
qinzhen <qinzhen@sensetime.com>
2022-04-20 13:00:57 +0800

    udpate_install

---
+++ install_1986.sh
@@ -0,0 +1,19 @@
+dir=/mnt/lustreold/share_data/sunweixuan/wheel_file
+
+pip install --user h5py
+pip install --user $dir/torch-1.8.1+cu101-cp38-cp38-linux_x86_64.whl
+pip install --user fvcore
+pip install --user $dir/torchvision-0.9.1+cu101-cp38-cp38-linux_x86_64.whl
+pip install --user simplejson
+pip install --user av
+pip install --user iopath
+pip install --user psutil
+pip install --user opencv-python
+pip install --user tensorboard
+pip install --user pytorchvideo
+pip install --user timm==0.4.12
+pip install --user $dir/detectron2-0.6+cu101-cp38-cp38-linux_x86_64.whl
+pip install --user sklearn
+
+# 可选, 如果要跑例子, 需要本地存一个ckpt, 1986
+# cp /mnt/lustre/share_data/sunweixuan/jx_vit_base_patch16_224_in21k-e5005f0a.pth /mnt/lustre/$USER/.cache/torch/hub/checkpoints
