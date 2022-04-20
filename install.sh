c0e88540effa3500e7402acff290ffa88ddab99a
qinzhen <qinzhen@sensetime.com>
2022-04-20 09:40:34 +0800

    update_install

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ pip install $dir/torch-1.8.1+cu101-cp38-cp38-linux_x86_64.whl
 pip install fvcore
 pip install $dir/torchvision-0.9.1+cu101-cp38-cp38-linux_x86_64.whl
 pip install simplejson
-# conda install av -c conda-forge
 pip install av
 pip install iopath
 pip install psutil
@@ -15,3 +14,6 @@ pip install pytorchvideo
 pip install timm==0.4.12
 pip install $dir/detectron2-0.6+cu101-cp38-cp38-linux_x86_64.whl
 pip install sklearn
+
+# 可选, 如果要跑例子, 需要本地存一个ckpt, 1986
+# cp /mnt/lustre/share_data/sunweixuan/jx_vit_base_patch16_224_in21k-e5005f0a.pth /mnt/lustre/$USER/.cache/torch/hub/checkpoints
